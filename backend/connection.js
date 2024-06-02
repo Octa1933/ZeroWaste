986c4d591b8ba13d6c6c303f7bc13c234f48c029
Akmal F Octavian <akmaloctavian98@gmail.com>
2024-06-02 18:04:13 +0700

    backend

--- backend/connection.js
+++ backend/connection.js
@@ -7,12 +7,4 @@ const db = mysql.createConnection({
   database: "zerowaste",
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL: " + err.stack);
-    return;
-  }
-  console.log("Connected to MySQL as id " + connection.threadId);
-});
-
 module.exports = db;
